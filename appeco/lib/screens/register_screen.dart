a188ce9f9341542693cb5263bb2797b6be2fabb2
Beatriz <biabatista1219@gmail.com>
2026-03-10 20:47:27 -0300

    apenas deixando com a mesma identidade visual da outra tela

--- appeco/lib/screens/register_screen.dart
+++ appeco/lib/screens/register_screen.dart
@@ -1,125 +1,240 @@
 import 'package:flutter/material.dart';
 
-class RegisterScreen extends StatelessWidget {
+class RegisterScreen extends StatefulWidget {
   const RegisterScreen({super.key});
+
+  @override
+  State<RegisterScreen> createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+
+  FocusNode nomeFocus = FocusNode();
+  FocusNode rmFocus = FocusNode();
+  FocusNode turmaFocus = FocusNode();
+  FocusNode emailFocus = FocusNode();
+  FocusNode senhaFocus = FocusNode();
+
+  @override
+  void initState() {
+    super.initState();
+
+    nomeFocus.addListener(() => setState(() {}));
+    rmFocus.addListener(() => setState(() {}));
+    turmaFocus.addListener(() => setState(() {}));
+    emailFocus.addListener(() => setState(() {}));
+    senhaFocus.addListener(() => setState(() {}));
+  }
+
+  @override
+  void dispose() {
+    nomeFocus.dispose();
+    rmFocus.dispose();
+    turmaFocus.dispose();
+    emailFocus.dispose();
+    senhaFocus.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 229, 248, 231),
+      backgroundColor: Colors.green[50],
       appBar: AppBar(
-        title: const Text("EcoTrack"),
+      
         centerTitle: true,
         automaticallyImplyLeading: false,
-        titleTextStyle: const TextStyle(
-          fontSize: 22,
-          color: Colors.white,
-          fontWeight: FontWeight.bold,
-        ),
         backgroundColor: Colors.green,
       ),
+
       body: Padding(
         padding: const EdgeInsets.all(24),
-        child: Column(
-          children: [
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
 
-            TextField(
-              decoration: InputDecoration(
-                labelText: "Nome",
-                prefixIcon: Icon(Icons.person),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(12),
+              TextField(
+                focusNode: nomeFocus,
+                decoration: InputDecoration(
+                  labelText: "Nome",
+
+                  prefixIcon: Icon(
+                    Icons.person,
+                    color: nomeFocus.hasFocus ? Colors.green : Colors.grey,
+                  ),
+
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                    borderSide: const BorderSide(
+                      color: Colors.green,
+                      width: 2,
+                    ),
+                  ),
+
+                  floatingLabelStyle: const TextStyle(
+                    color: Colors.green,
+                  ),
                 ),
               ),
-            ),
 
-            const SizedBox(height: 15),
+              const SizedBox(height: 15),
 
-            TextField(
-              decoration: InputDecoration(
-                labelText: "RM",
-                prefixIcon: Icon(Icons.badge),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(12),
+              TextField(
+                focusNode: rmFocus,
+                decoration: InputDecoration(
+                  labelText: "RM",
+
+                  prefixIcon: Icon(
+                    Icons.badge,
+                    color: rmFocus.hasFocus ? Colors.green : Colors.grey,
+                  ),
+
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                    borderSide: const BorderSide(
+                      color: Colors.green,
+                      width: 2,
+                    ),
+                  ),
+
+                  floatingLabelStyle: const TextStyle(
+                    color: Colors.green,
+                  ),
                 ),
               ),
-            ),
 
-            const SizedBox(height: 15),
+              const SizedBox(height: 15),
 
-            TextField(
-              decoration: InputDecoration(
-                labelText: "Turma",
-                prefixIcon: Icon(Icons.groups),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(12),
+              TextField(
+                focusNode: turmaFocus,
+                decoration: InputDecoration(
+                  labelText: "Turma",
+
+                  prefixIcon: Icon(
+                    Icons.groups,
+                    color: turmaFocus.hasFocus ? Colors.green : Colors.grey,
+                  ),
+
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                    borderSide: const BorderSide(
+                      color: Colors.green,
+                      width: 2,
+                    ),
+                  ),
+
+                  floatingLabelStyle: const TextStyle(
+                    color: Colors.green,
+                  ),
                 ),
               ),
-            ),
 
-            const SizedBox(height: 15),
+              const SizedBox(height: 15),
 
-            TextField(
-              decoration: InputDecoration(
-                labelText: "Email",
-                prefixIcon: Icon(Icons.email),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(12),
+              TextField(
+                focusNode: emailFocus,
+                decoration: InputDecoration(
+                  labelText: "Email",
+
+                  prefixIcon: Icon(
+                    Icons.email,
+                    color: emailFocus.hasFocus ? Colors.green : Colors.grey,
+                  ),
+
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                    borderSide: const BorderSide(
+                      color: Colors.green,
+                      width: 2,
+                    ),
+                  ),
+
+                  floatingLabelStyle: const TextStyle(
+                    color: Colors.green,
+                  ),
                 ),
               ),
-            ),
 
-            const SizedBox(height: 15),
+              const SizedBox(height: 15),
 
-            TextField(
-              obscureText: true,
-              decoration: InputDecoration(
-                labelText: "Senha",
-                prefixIcon: Icon(Icons.lock),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(12),
+              TextField(
+                focusNode: senhaFocus,
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: "Senha",
+
+                  prefixIcon: Icon(
+                    Icons.lock,
+                    color: senhaFocus.hasFocus ? Colors.green : Colors.grey,
+                  ),
+
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                    borderSide: const BorderSide(
+                      color: Colors.green,
+                      width: 2,
+                    ),
+                  ),
+
+                  floatingLabelStyle: const TextStyle(
+                    color: Colors.green,
+                  ),
                 ),
               ),
-            ),
 
-            const SizedBox(height: 25),
+              const SizedBox(height: 30),
 
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.green,
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 50,
-                  vertical: 14,
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.green,
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 60,
+                    vertical: 14,
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
                 ),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(12),
-                ),
-              ),
-              onPressed: () {},
-              child: const Text(
-                "Criar conta",
-                style: TextStyle(
-                  fontSize: 16,
-                  color: Colors.white,
-                ),
-              ),
-            ),
 
-            const SizedBox(height: 15),
+                onPressed: () {},
 
-            TextButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: const Text(
-                "Já tenho conta",
-                style: TextStyle(
-                  fontSize: 14,
-                  color: Colors.green
+                child: const Text(
+                  "Cadastrar",
+                  style: TextStyle(fontSize: 16),
                 ),
               ),
-            )
-          ],
+
+              const SizedBox(height: 10),
+
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text("Já tenho conta"),
+              ),
+
+            ],
+          ),
         ),
       ),
     );
